35dcc4155281b695b85e2ea6e432914c3244503c
Vladislav <wlladisla@gmail.com>
2024-10-04 00:00:56 +0300

    feature: implement rename and delete funcs

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import path from 'path';
 const rename = async () => {
-    // Write your code here 
+    try {
+        const __dirname = import.meta.dirname;
+
+        const filesDir = path.join(__dirname, 'files');
+        const currPathToRename = path.join(filesDir, 'wrongFilename.txt');
+        const newPathToRename = path.join(filesDir, 'properFilename.md');
+
+        if (!fs.existsSync(currPathToRename) || fs.existsSync(newPathToRename)) {
+            throw new Error('FS operation failed');
+        }
+        await fs.promises.rename(currPathToRename, newPathToRename);
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
 await rename();
