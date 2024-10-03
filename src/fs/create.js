120718eab310b7b09d2a4c9bed6f3b5e3fd2c16f
Vladislav <wlladisla@gmail.com>
2024-10-03 23:27:01 +0300

    feature: add copy and create functions

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import path from 'path';
 const create = async () => {
-    // Write your code here 
+    const __dirname = import.meta.dirname;
+    const dir = path.join(__dirname, 'files');
+    try {
+        if (fs.existsSync(path.join(dir, 'fresh.txt'))) {
+            const err = new Error('FS operation failed');
+            throw err;
+        }
+        fs.writeFile(path.join(dir, 'fresh.txt'), 'I am fresh and young', () => {
+            console.log('File created');
+        });
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
 await create();
