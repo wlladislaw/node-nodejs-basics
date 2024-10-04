b26393d106c23796bc272b1c2aa9a51bdd69fc69
Vladislav <wlladisla@gmail.com>
2024-10-04 10:28:28 +0300

    feature: implement cli subtasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+    const currArgs = process.argv;
+    const args = [];
+    for (let index = 0; index < currArgs.length; index++) {
+        const element = currArgs[index];
+        if (element.startsWith('--')) {
+            args.push(element.slice(2) + ' ' + 'is' + ' ' + currArgs[index + 1]);
+            continue;
+        }
+    }
+    console.log(args.join(', '));
 };
 
 parseArgs();
