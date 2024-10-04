7eb9a988d21fb9d66c20ce9c42c8f8c80b89f999
Vladislav <wlladisla@gmail.com>
2024-10-04 09:33:28 +0300

    feature: add list and read funcs

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import path from 'path';
 const read = async () => {
-    // Write your code here 
+    try {
+        const __dirname = import.meta.dirname;
+
+        const filesDir = path.join(__dirname, 'files');
+        const pathFileToRead = path.join(filesDir, 'fileToRead.txt');
+
+        if (!fs.existsSync(pathFileToRead)) {
+            throw new Error('FS operation failed');
+        }
+        const fileContent = await fs.promises.readFile(pathFileToRead, { encoding: 'utf8' });
+        console.log(fileContent);
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
 await read();
