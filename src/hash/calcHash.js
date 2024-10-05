f1f4cd7de022a9a1f16306072298d6b7a6dfe3fa
Vladislav <wlladisla@gmail.com>
2024-10-05 22:18:19 +0300

    feature : add func for hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import path from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __dirname = import.meta.dirname;
+    const fileDir = path.join(__dirname, 'files');
+
+    const filePath = path.join(fileDir, 'fileToCalculateHashFor.txt');
+    const hash = createHash('sha256');
+    const input = createReadStream(filePath);
+
+    input.on('readable', () => {
+        const data = input.read();
+
+        if (data) hash.update(data);
+        else {
+            console.log(`Hash hex - ${hash.digest('hex')}`);
+        }
+    });
 };
 
 await calculateHash();
