ee7a6c96d1df36ade29fd92180797169380abf5c
Vladislav <wlladisla@gmail.com>
2024-10-06 14:16:46 +0300

    feature: add funcs for streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform } from 'stream';
 const transform = async () => {
-    // Write your code here 
+    const stream = new Transform({
+        transform(chunk, encoding, cb) {
+            cb(null, chunk.toString().split('').reverse().join(''));
+        },
+    });
+    console.log('write here text and press Enter , for end - Ctrl+D or Ctrl+Z');
+
+    process.stdin.pipe(stream).pipe(process.stdout);
+
+    stream.on('close', () => {
+        console.log('\nDone!');
+    });
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import { createWriteStream } from 'fs';
 const write = async () => {
-    // Write your code here 
+    const __dirname = import.meta.dirname;
+    const fileDir = path.join(__dirname, 'files');
+
+    const filePath = path.join(fileDir, 'fileToWrite.txt');
+    const stream = createWriteStream(filePath, { flags: 'a' });
+    console.log('write here text for file, for end press - Ctrl+D or Ctrl+Z');
+
+    process.stdin.pipe(stream);
+
+    stream.on('close', () => {
+        console.log('\n Done!');
+    });
 };
 
 await write();
