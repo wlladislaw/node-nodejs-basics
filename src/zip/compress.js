d33b821b4f0c3c378569b02afbc8a8677f20c422
Vladislav <wlladisla@gmail.com>
2024-10-06 19:14:24 +0300

    feature: add zip functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import path from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
 const compress = async () => {
-    // Write your code here 
+    const __dirname = import.meta.dirname;
+    const fileDir = path.join(__dirname, 'files');
+    const gz = createGzip();
+    const filePath = path.join(fileDir, 'fileToCompress.txt');
+    const input = createReadStream(filePath);
+    const res = createWriteStream(path.join(fileDir, 'archive.gz'));
+    input.pipe(gz).pipe(res);
+    res.on('finish', () => {
+        console.log('file compressed to archive.gz in files directory');
+    });
+    gz.on('error', (err) => {
+        console.error(`error compression: ${err.message}`);
+    });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import path from 'path';
+import { createReadStream, createWriteStream, existsSync } from 'fs';
+import { createGunzip } from 'zlib';
 const decompress = async () => {
-    // Write your code here 
+    const __dirname = import.meta.dirname;
+    const fileDir = path.join(__dirname, 'files');
+    const gunzip = createGunzip();
+    const filePath = path.join(fileDir, 'archive.gz');
+    if (!existsSync(filePath)) {
+        console.log('Compress archive for decompress func!!!');
+        return;
+    }
+    const input = createReadStream(filePath);
+
+    input.on('error', (error) => {
+        console.error(error.message);
+    });
+    const res = createWriteStream(path.join(fileDir, 'fileToCompress.txt'));
+
+    input.pipe(gunzip).pipe(res);
+    res.on('finish', () => {
+        console.log('file decompressed in files directory');
+    });
 };
 
 await decompress();
